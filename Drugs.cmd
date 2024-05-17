d1593ad38b3046721608adb0475459d5cdde2997
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-16 15:25:58 -1000

    Update Drugs.cmd

--- Drugs.cmd
+++ Drugs.cmd
@@ -4,10 +4,6 @@ cls
 mode 40,25
 title "Drugs"
 @cecho off 
-
-@set "apk=https://www.apkmirror.com/apk/venmo/venmo-venmo/venmo-venmo-10-39-1-release/venmo-10-39-1-android-apk-download/download/?key=29ffd86c00b5ea8598bba888cd442497709f18d6&forcebaseapk=true"
-@set "app=https://apps.apple.com/us/app/venmo/id351727428"
-
 cecho {04}Kiaaina J A Medeiros : American_Savings_Bank{\n}
 cecho {05}$5:cigarette{\n}$5:1/8 Ciannabis{\n}$5:Vicaden{\n}$10:Oxycotten{07}
 pause
