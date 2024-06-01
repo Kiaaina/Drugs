03cf250904ecb468e26b171f6849fe9b835a6ad1
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-31 15:27:16 -1000

    Update Drugs.cmd

--- Drugs.cmd
+++ Drugs.cmd
@@ -4,14 +4,14 @@ cls
 mode 40,25
 title "Drugs"
 @echo off 
-set "cecho=.\cecho.exe"
-%cecho% {04}Kiaaina J A Medeiros : American_Savings_Bank{\n}
-%cecho% {05}$5:cigarette{\n}$5:1/8 Ciannabis{\n}$5:Vicaden{\n}$10:Oxycotten{07}
+
+cecho {04}Kiaaina J A Medeiros : American_Savings_Bank{\n}
+cecho {05}$5:cigarette{\n}$5:1/8 Ciannabis{\n}$5:Vicaden{\n}$10:Oxycotten{07}
 pause
 
 goto 7L
 
 :7L
-%cecho% {05}Made_By_7L{02}
+cecho {05}Made_By_7L{02}
 @timeout /t 5
 goto top
